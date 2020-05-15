84ed992fb4784716fd01db7c881052ab1d4267f3
DonaldJethro <61614298+DonaldJethro@users.noreply.github.com>
2020-05-15 17:52:07 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,35 @@
+/**
+	* @module config/bot
+	* @desc Before editing, this file should be cloned from bot.js.template to bot.js
+	* @author WizardCM <bots@wizardcm.com>
+*/
+
+/**
+	@type {Object.<string, string>}
+*/
+module.exports = {
+	/**
+	 * @desc Bot username without discriminator
+	 * @default "MyTimeBot"
+	 * @type string
+	 */
+	name: "Cosmic Time Bot",
+	/**
+	 * @desc Global bot command prefix
+	 * @default "?"
+	 * @type string
+	*/
+	prefix: "?",
+	/** 
+	 * @desc Discord API bot token (keep it secret)
+	 * @default "InsertYourTokenHere"
+	 * @type string
+	 */
+	token:"NzEwNDUwNjU4NzcxNDY4Mzg4.Xr0o1w.CVEUCYXdlq8oTQZQ09FTXswgKpg",
+	/** 
+	 * @desc Global bot response heading
+	 * @default "\uD83D\uDD52 Discord Time Bot"
+	 * @type string
+	 */
+	title: " Cosmic Time Bot"
+};
